f6906629bd279b754e959693c73cac9a5fb61bbf
Ankit628792 <ankit628792@gmail.com>
2025-02-15 10:35:12 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="w-full min-h-screen bg-gray-50 dark:bg-gray-950 grid place-items-center">
+
+      <h1 className="text-7xl font-semibold text-black data:text-white">Qrazy</h1>
+
+    </section>
+  );
+}
